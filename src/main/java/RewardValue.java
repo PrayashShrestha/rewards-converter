f7c45e3715e076456627e6411d6e3a5f3d070ab4
prayash27 <prayash.shrestha@wiseyak.com>
2024-05-29 23:26:41 -0500

    sign altered in conversion logics. Tested with 4 cases

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,16 +1,16 @@
 public class RewardValue {
     private final double cashValue;
     private final int milesValue;
-    private final double CONVERSION_RATE = 0.0035;
+    public static final double CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue){
         this.cashValue = cashValue;
-        this.milesValue = (int) (this.cashValue * CONVERSION_RATE);
+        this.milesValue = (int) (this.cashValue / CONVERSION_RATE);
     }
 
     public RewardValue(int milesValue){
         this.milesValue = milesValue;
-        this.cashValue = milesValue / CONVERSION_RATE;
+        this.cashValue = milesValue * CONVERSION_RATE;
     }
 
     public double getCashValue() {
